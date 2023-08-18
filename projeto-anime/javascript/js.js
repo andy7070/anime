65eb77347988aade660e566814b15d612e408a4a
Anderson Felipe <136264571+andy7070@users.noreply.github.com>
2023-08-18 20:25:51 -0300

    começando projeto

---
+++ projeto-anime/javascript/js.js
@@ -0,0 +1,13 @@
+
+let display = document.getElementById('teste')
+
+function clickMenu(){
+if(teste.style.display == 'none'){
+teste.style.display = ' block'}else {
+
+teste.style.display = ' none'}
+
+display.style.transform = translateX(30)
+
+
+}
