3328936cc5afbc9b335b2bc71fa71eb0fca0832b
Anderson Felipe <136264571+andy7070@users.noreply.github.com>
2023-08-18 23:44:10 -0300

    ainda nos menus

--- projeto-anime/javascript/js.js
+++ projeto-anime/javascript/js.js
@@ -3,15 +3,17 @@ if(animes.style.display == 'none'){
   animes.style.display = 'block'
 } else {
   animes.style.display = 'none'
-}
- 
-}
-
-
+}}
 
 function clickMenu2(){
 if(animes2.style.display == 'none'){
 animes2.style.display = 'block'}else{
 animes2.style.display = 'none'}}
+
+function clickMenu3(){
+if(animes3.style.display == 'none'){
+    animes3.style.display = 'block'
+} else{
+animes3.style.display = 'none'}}
 
 
