92cf9cdc0d86fd3ab781b8be9c86ba9c1f70ed3a
Anderson Felipe <136264571+andy7070@users.noreply.github.com>
2023-08-19 04:07:34 -0300

    slider

--- projeto-anime/javascript/js.js
+++ projeto-anime/javascript/js.js
@@ -39,3 +39,19 @@ function clickMenu6(){
   function click(){
    document.style.transform = 'rotate(90)deg'
 }
+
+
+const box = document.querySelector('.slider')
+const b = document.querySelectorAll('.slider img')
+
+let c = 0
+
+
+function slider(){
+    c++
+  if(c == b.length - 1){
+c = 0 }else{
+    box.style.transform = `translateX(${-c * 195}px)`
+ }}
+
+ setInterval (slider, 3000)
