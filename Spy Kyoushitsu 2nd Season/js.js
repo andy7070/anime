bf2b9b30cf76e3245709370d375c9c182c0c63c3
Anderson Felipe <136264571+andy7070@users.noreply.github.com>
2023-08-23 05:21:54 -0300

    mas uma page de anime criada

---
+++ Spy Kyoushitsu 2nd Season/js.js
@@ -0,0 +1,56 @@
+function clickMenu(){
+  
+  if(animes.style.display == 'block'){
+    animes.style.display = 'none'
+  } else {
+    animes.style.display = 'block'
+  }
+
+   
+  }
+  
+  function clickMenu2(){
+  if(animes2.style.display == 'block'){
+  animes2.style.display = 'none'}else{
+  animes2.style.display = 'block'}}
+  
+  function clickMenu3(){
+  if(animes3.style.display == 'block'){
+      animes3.style.display = 'none'
+  } else{
+  animes3.style.display = 'block'}}
+  
+  
+  function clickMenu4(){
+  if(animes4.style.display == 'block'){
+  animes4.style.display = 'none'}else{
+   animes4.style.display = ' block'}}
+  
+   function clickMenu5(){
+  if(animes5.style.display == 'block'){
+   animes5.style.display = 'none' }else{
+    animes5.style.display = 'block' }
+  }
+  
+  
+  function clickMenu6(){
+   if(animes6.style.display == 'block'){
+     animes6.style.display= 'none'}else{
+    animes6.style.display = 'block'}}
+  
+
+
+  function setaEp(){
+    if(epep.style.display == 'block'){
+    epep.style.display = 'none'} else{
+    epep.style.display = 'block'}
+
+    epep.style.transition = '0.7s'
+
+    if(epseta.style.transform == 'rotateX(0deg)'){
+    epseta.style.transform = 'rotateX(180deg)'}else{
+    epseta.style.transform = 'rotateX(0deg)'}
+
+    
+  }
+    
