3e2a0bf68b1ecdded2929d38db48429572b9ece5
Anderson Felipe <136264571+andy7070@users.noreply.github.com>
2023-08-29 17:58:08 -0300

    teste

--- desktop.js
+++ desktop.js
@@ -1,7 +1,4 @@
 
-
-
-
 function menu1(){
   if(animes2_pc.style.display == 'block'){
   animes2_pc.style.display = 'none'
@@ -10,4 +7,43 @@ animes2_pc.style.display = 'block'
 }
 
 }
+
+function menu2(){
+  if(animes3_pc.style.display == 'block'){
+  animes3_pc.style.display = 'none'
+} else {
+animes3_pc.style.display = 'block'
+}
+
+}
+
+function menu3(){
+  if(animes4_pc.style.display == 'block'){
+  animes4_pc.style.display = 'none'
+} else {
+animes4_pc.style.display = 'block'
+}
+
+}
+
+function menu4(){
+  if(animes5_pc.style.display == 'block'){
+  animes5_pc.style.display = 'none'
+} else {
+animes5_pc.style.display = 'block'
+}
+
+}
+
+function menu5(){
+  if(animes6_pc.style.display == 'block'){
+  animes6_pc.style.display = 'none'
+} else {
+animes6_pc.style.display = 'block'
+}
+
+}
+
+
+
 
