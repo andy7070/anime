f335eb54f2856f8dbcf0116969d3134568f5c577
Anderson Felipe <136264571+andy7070@users.noreply.github.com>
2023-09-09 15:24:46 -0300

    js

--- AI No Idenshi/desktop.js
+++ AI No Idenshi/desktop.js
@@ -52,6 +52,8 @@ const frames = document.getElementById('frames')
 const clickTrailer = document.getElementById('click')
 const teste = document.getElementById('sair')
 const teste2 = document.getElementById('click2')
+const teste3 = document.getElementById('iframes')
+const setafechar = document.getElementById('icon_fechar')
 let a = 0
 
 
@@ -70,12 +72,18 @@ seta.addEventListener('click', function() {
 })
 
 teste2.addEventListener('click', function(){
- if(frames.style.display == 'block'){
- frames.style.display = 'none'
-}else{
- frames.style.display = 'block'
-}
+  if(frames.style.display == 'block'){
+  frames.style.display = 'none'
+ }else{
+  frames.style.display = 'block'
+ }
+  if(setafechar.style.display == 'block'){
+  setafechar.style.display = 'none'
+  } else {
+  setafechar.style.display = 'block'
+  }
 })
+
 
 
 clickTrailer.addEventListener('click', function(){
@@ -86,8 +94,10 @@ frames.style.display = 'block'
 }
 clickTrailer.style.transition = ' transform 5s ease-in-out linear'
 
-
+            
 })
+
+
 
 
 
